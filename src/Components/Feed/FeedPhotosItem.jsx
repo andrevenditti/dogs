5236cd34c577ab7ac1a37973828eeb0aff1fd56c
Andre Venditti <am7venditti@gmail.com>
2025-06-08 20:18:49 -0300

    feat: added delete photo; added skeleton before loading image

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from '../Feed/FeedPhotosItem.module.css'
+import { Image } from '../Helper/Image'
 
 export const FeedPhotosItem = ({ photo, onClickPhoto }) => {
   return (
     <li className={styles.photo} onClick={() => onClickPhoto(photo)}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.views}>{photo.acessos}</span>
     </li>
   )
--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -2,19 +2,28 @@ import React from 'react'
 import styles from './PhotoContent.module.css'
 import { Link } from 'react-router-dom'
 import { PhotoComments } from './PhotoComments'
+import { UserContext } from '../../Contexts/UserContext'
+import { PhotoDelete } from './PhotoDelete'
+import { Image } from '../Helper/Image'
 
 export const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext)
   const { photo, comments } = data
 
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.photoDetails}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username == photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+
             <span className={styles.views}>{photo.acessos}</span>
           </p>
           <h1 className="title">
